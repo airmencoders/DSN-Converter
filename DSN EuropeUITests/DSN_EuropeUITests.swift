1f0ec9f35ad78da7bb12c298fa188d96a4140513
christian.brechbuhl <cbrechbuhl@gmail.com>
2020-03-15 12:29:49 -0400

    Added default dsnService for testing

--- DSN EuropeUITests/DSN_EuropeUITests.swift
+++ DSN EuropeUITests/DSN_EuropeUITests.swift
@@ -12,7 +12,9 @@ class DSN_EuropeUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        let app = XCUIApplication()
+                        app.launchArguments.append("ui-testing")
+                        app.launch()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
