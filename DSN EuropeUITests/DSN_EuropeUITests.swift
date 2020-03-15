3a1249616e5485129fe1aca03ee8e569dcb1eaa3
christian.brechbuhl <cbrechbuhl@gmail.com>
2020-03-15 11:42:10 -0400

    Updated tests

--- DSN EuropeUITests/DSN_EuropeUITests.swift
+++ DSN EuropeUITests/DSN_EuropeUITests.swift
@@ -31,6 +31,48 @@ class DSN_EuropeUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+       func testRamstein480() {
+           // UI tests must launch the application that they test.
+           let app = XCUIApplication()
+           app.launch()
+    
+           app.buttons["4 GHI"].tap()
+           app.buttons["8 TUV"].tap()
+           app.buttons["0 +"].tap()
+           
+           let button = app.buttons["2 ABC"]
+           button.tap()
+           
+           let button2 = app.buttons["5 JKL"]
+           button2.tap()
+           button.tap()
+           button2.tap()
+           app.buttons["phoneIcon"].tap()
+           app.staticTexts["Ramstein AB"].tap()
+           app.navigationBars["DSN Lookup"].buttons["Stop"].tap()
+           
+       }
+       
+       func testAviano(){
+           
+           let app = XCUIApplication()
+           app.launch()
+           app.buttons["6 MNO"].tap()
+           
+           let button = app.buttons["3 DEF"]
+           button.tap()
+           
+           let button2 = app.buttons["2 ABC"]
+           button2.tap()
+           app.buttons["1"].tap()
+           button2.tap()
+           button.tap()
+           app.buttons["4 GHI"].tap()
+           app.buttons["phoneIcon"].tap()
+           app.staticTexts["Aviano AB"].tap()
+           app.navigationBars["DSN Lookup"].buttons["Stop"].tap()
+           
+       }
 
     func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
