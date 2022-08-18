f53ba0c53066c1cbef3fb37440ec9417160ba909
Roger <rogerrivl.dev@gmail.com>
2022-08-18 23:35:01 +0200

    More Updates.

---
+++ DSN Converter/Main/Operator/OperatorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OperatorTableViewCell.swift
+//  DSN Converter
+//
+//  Created by Roger Rivl on 8/18/22.
+//  Copyright © 2022 ForceMobileStudios. All rights reserved.
+//
+
+import UIKit
+
+class OperatorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DSN Converter/Main/Operator/OperatorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  OperatorViewController.swift
+//  DSN Converter
+//
+//  Created by Roger Rivl on 8/18/22.
+//  Copyright © 2022 ForceMobileStudios. All rights reserved.
+//
+
+import UIKit
+
+class OperatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
